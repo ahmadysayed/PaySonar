18866795cd0b09c0b0d682bac1b59e8083664982
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-11-26 22:51:46 +0100

    Header Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-    </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,60 @@
 import React from 'react';
-
+import { LinkContainer } from 'react-router-bootstrap';
 
 function Header() {
-
   return (
-    <div class="border-b bg-[#F1F2F6] border-[#B4BBBA4D] ">
-        <header class="container flex items-center justify-between w-full py-8 mx-auto">
-            <div class="flex items-center gap-x-12">
-            <a routerLinkActive="" routerLink="/">
-                <div class="w-[67px] h-[67px] bg-teal-600 rounded-full ">
-                </div>
-            </a>
-            <div>
-                <ul class="flex items-center gap-x-5 ">
-                <li class="text-[24px] hover:cursor-pointer !font-normal"><a routerLinkActive="" routerLink="/history">
-                    Home</a></li>
-                <li class="text-[24px] hover:cursor-pointer !font-normal"><a routerLinkActive="" routerLink="/profile">
-                    Profile</a></li>
-                <li class="text-[24px] hover:cursor-pointer !font-normal"><a routerLinkActive="" routerLink="/summary">
-                    Summary</a></li>
-                <li class="text-[24px] hover:cursor-pointer !font-normal"><a routerLinkActive="active" routerLink="/about">
-                    About</a></li>
-                <li class="text-[24px] hover:cursor-pointer !font-normal"><a routerLinkActive="active" routerLink="/contact">
+    <div className="border-b bg-[#F1F2F6] border-[#B4BBBA4D] ">
+      <header className="container flex items-center justify-between w-full py-8 mx-auto">
+        <div className="flex items-center gap-x-12">
+          <LinkContainer to="/">
+            <div className="w-[67px] h-[67px] bg-teal-600 rounded-full "></div>
+          </LinkContainer>
+          <div>
+            <ul className="flex items-center gap-x-5 ">
+              <li className="text-[24px] hover:cursor-pointer !font-normal">
+                <LinkContainer to="/history">
+                  <span>Home</span>
+                </LinkContainer>
+              </li>
+              <li className="text-[24px] hover:cursor-pointer !font-normal">
+                <LinkContainer to="/profile">
+                  <span>Profile</span>
+                </LinkContainer>
+              </li>
+              <li className="text-[24px] hover:cursor-pointer !font-normal">
+                <LinkContainer to="/summary">
+                  <span>Summary</span>
+                </LinkContainer>
+              </li>
+              <li className="text-[24px] hover:cursor-pointer !font-normal">
+                <LinkContainer to="/about">
+                  <span>About</span>
+                </LinkContainer>
+              </li>
+              <li className="text-[24px] hover:cursor-pointer !font-normal">
+                <LinkContainer to="/contact">
+                  <span>Contact</span>
+                </LinkContainer>
+              </li>
+            </ul>
+          </div>
+        </div>
 
-                    contact</a></li>
-                </ul>
-            </div>
-            </div>
-
-            <div class="flex items-center gap-x-5">
-            <button
-                class="px-3 py-2 !font-normal transition-colors duration-300 border-2 rounded-md border-paysonarTeal-normal hover:text-white hover:bg-paysonarTeal-normal ">Sign
-                In</button>
-            <button
-                class="px-3 py-2 !font-normal text-white transition-colors duration-300 border-2 rounded-md border-paysonarTeal-normal hover:text-black bg-paysonarTeal-normal hover:bg-white ">Sign
-                Up</button>
-            </div>
-        </header>
+        <div className="flex items-center gap-x-5">
+          <LinkContainer to="/login">
+            <button className="px-3 py-2 !font-normal transition-colors duration-300 border-2 rounded-md border-paysonarTeal-normal hover:text-white hover:bg-paysonarTeal-normal ">
+                Sign In
+            </button>
+          </LinkContainer>
+          <LinkContainer to="/signup">
+            <button className="px-3 py-2 !font-normal text-white transition-colors duration-300 border-2 rounded-md border-paysonarTeal-normal hover:text-black bg-paysonarTeal-normal hover:bg-white ">
+                Sign Up
+            </button>
+          </LinkContainer>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
