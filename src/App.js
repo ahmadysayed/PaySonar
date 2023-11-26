cc91809972356b34306ca8bd4b5ef0de9f6ebf7e
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-11-26 22:25:42 +0100

    PaySonar Developement

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div class="flex items-center justify-center w-full h-screen text-4xl text-white bg-paysonarTeal-normal">
+  About Page
+</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        paysonarTeal: {
+          normal: "#05877D",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
